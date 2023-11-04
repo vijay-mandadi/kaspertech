b7c727e800403c305a4174773b647d1e85f83658
vijay-mandadi <vijaymandadi26@gmail.com>
2023-11-04 10:04:45 +0000

    solution

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,240 @@
+import { CgMenuGridO } from "react-icons/cg";
+import "./index.css";
+
+const LandingPage = () => (
+  <>
+    <div className="bg-container">
+      <nav className="navbar-container">
+        <ul className="navbar-list-container">
+          <li className="container-14">
+            <img
+              src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/dora-1@2x.png"
+              alt="logo"
+              className="logo"
+            />
+            <div className="dot" />
+          </li>
+          <li className="navbar-listitem">Explore</li>
+          <li className="navbar-listitem">Company</li>
+          <li className="navbar-listitem">Support</li>
+        </ul>
+        <div className="sign-in-container">
+          <p className="member-description">Become a member</p>
+          <button className="sign-in-btn">Sign in</button>
+        </div>
+      </nav>
+      <nav className="container-15">
+        <div className="container-14">
+          <img
+            src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/dora-1@2x.png"
+            alt="logo"
+            className="logo"
+          />
+          <div className="dot" />
+        </div>
+        <CgMenuGridO className="menu-icon" />
+      </nav>
+      <div className="page-description-container">
+        <h1 className="page-heading">
+          Learn from your <br /> favorite creators <br /> about{" "}
+          <span className="heading-span">fantasy reports</span>
+        </h1>
+        <div className="video-call-outer-container">
+          <div className="video-call-inner-container">
+            <p className="ratio">1:1</p>
+            <p className="video-call-heading">video calls</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="bg-card-container">
+      <img
+        src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/img-1@2x.png"
+        alt="img"
+        className="banner"
+      />
+    </div>
+    <div className="bg-creators-container">
+      <div className="creators-container">
+        <h1 className="knowledge-insight">
+          <span className="get-direct">Get direct</span>
+          <br />
+          knowledge and insight.
+        </h1>
+        <div className="row-images">
+          <img
+            src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/img-card-1@2x.png"
+            alt="imgcard"
+            className="image-card"
+          />
+          <img
+            src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/img-card-2@2x.png"
+            alt="imgcard"
+            className="image-card"
+          />
+          <img
+            src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/img-card-3@2x.png"
+            alt="imgcard"
+            className="image-card"
+          />
+        </div>
+        <div className="creators-turn-page-container">
+          <div className="creators-count-container">
+            <div className="single-item">
+              <p className="count">300</p>
+              <p className="creators">creators</p>
+            </div>
+            <div className="single-item">
+              <p className="count">14.4k</p>
+              <p className="creators">users</p>
+            </div>
+            <div className="single-item">
+              <p className="count">9.11k</p>
+              <p className="creators">session</p>
+            </div>
+          </div>
+          <div className="explore-container">
+            <p className="creators">Explore the marketplace</p>
+            <img
+              src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/arrow-icon-1@2x.png"
+              alt="arrow"
+              className="arrow"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container-1">
+      <div className="container-2">
+        <div className="container-3">
+          <h1 className="question-heading">
+            Why become a <br /> creator?
+          </h1>
+          <p className="para-1">
+            Engage with your supporter and subscriber to sell your skills,
+            knowledge and passions
+          </p>
+          <button type="button" className="btn-1">
+            Become a creator
+          </button>
+        </div>
+        <div className="container-4">
+          <div className="container-5">
+            <div className="container-6">
+              <img
+                src="https://i.ibb.co/BPS8NH5/engaze-svg.jpg"
+                alt="circle"
+                className="circle"
+              />
+              <p className="para-2">Engagement</p>
+              <p className="para-3">
+                Engage on a deeper level with the fans that matter most.
+              </p>
+            </div>
+            <div className="container-6">
+              <img
+                src="https://i.ibb.co/1K2GxGm/autonomy-svg.jpg"
+                alt="circle"
+                className="circle"
+              />
+              <p className="para-2">Autonomy</p>
+              <p className="para-3">
+                Full autonomy on when and who you talk to.
+              </p>
+            </div>
+          </div>
+          <div className="container-5">
+            <div className="container-6">
+              <img
+                src="https://i.ibb.co/n3y9q08/free-svg.jpg"
+                alt="circle"
+                className="circle"
+              />
+              <p className="para-2">Free</p>
+              <p className="para-3">No monthly fee or sign-up fee</p>
+            </div>
+            <div className="container-6">
+              <img
+                src="https://i.ibb.co/fxJszkR/earn-svg.jpg"
+                alt="circle"
+                className="circle"
+              />
+              <p className="para-2">Earn</p>
+              <p className="para-3">
+                Add a new source of income that you can control.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="container-8">
+      <div className="container-7">
+        <p className="para-4">Engage without limits</p>
+        <p className="para-5">
+          Join a growing community and talk directly with your favorites
+          creators
+        </p>
+        <button type="buttton" className="btn-2">
+          Get Started
+        </button>
+      </div>
+      <footer className="container-9">
+        <div className="container-10">
+          <div className="container-14">
+            <img
+              src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/dora-1@2x.png"
+              alt="logo"
+              className="logo"
+            />
+            <div className="dot" />
+          </div>
+          <div className="container-11">
+            <img
+              className="facebook"
+              alt="Facebook"
+              src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/facebook-1@2x.png"
+            />
+            <img
+              className="facebook"
+              alt="Twitter"
+              src="https://cdn.animaapp.com/projects/6544d7bf05a45a1076c69f17/releases/6544d7e75a67074db4e35b5c/img/twitter-1@2x.png"
+            />
+            <img
+              className="facebook"
+              alt="Twitter"
+              src="https://i.ibb.co/b6mgXnv/instagram.jpg"
+            />
+          </div>
+        </div>
+        <div className="container-12">
+          <div className="container-13">
+            <p className="text-1">About</p>
+            <p className="text-2">How it works</p>
+            <p className="text-2">Company</p>
+            <p className="text-2">Help & Support</p>
+          </div>
+          <div className="container-13">
+            <p className="text-1">For creators</p>
+            <p className="text-2">How can i get paid</p>
+            <p className="text-2">Become a creator</p>
+          </div>
+          <div className="container-13">
+            <p className="text-1">Learn</p>
+            <p className="text-2">Blog</p>
+            <p className="text-2">Cookies policy</p>
+            <p className="text-2">Privacy policy</p>
+          </div>
+          <div className="container-13">
+            <p className="text-1">Support</p>
+            <p className="text-2">Service status</p>
+            <p className="text-2">Report abuse</p>
+            <p className="text-2">Dispute a payment</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </>
+);
+
+export default LandingPage;
